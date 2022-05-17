7957ee1b7bffb2c4ea9e405210e34d35d7ea93e3
Antonio Montero <019101861@uandina.edu.pe>
2022-05-17 13:57:56 -0500

    v2

---
+++ app/src/main/java/com/example/appmaravillas/splash.java
@@ -0,0 +1,32 @@
+package com.example.appmaravillas;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class splash extends AppCompatActivity {
+
+
+    public static final long SPLASH_SCREEN_DELAY=1000;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+
+        TimerTask time= new TimerTask() {
+            @Override
+            public void run() {
+                Intent intent=new Intent().setClass(splash.this,main.class);
+                startActivity(intent);
+                finish();
+            }
+        };
+        Timer timer=new Timer();
+        timer.schedule(time,SPLASH_SCREEN_DELAY);
+    }
+}
